c7722e605078857910f264f8c295990f7b22bfd9
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-03-04 20:51:12 +0200

    lesson 23

---
+++ src/react-query/hooks/usePosts.ts
@@ -0,0 +1,38 @@
+import {
+  InfiniteData,
+  QueryFunction,
+  useInfiniteQuery,
+  useQuery,
+} from "@tanstack/react-query";
+import axios from "axios";
+export interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+interface PostQuery {
+  pageSize: number;
+}
+
+const usePosts = (query: PostQuery) =>
+  useInfiniteQuery<InfiniteData<Post[], Error>>({
+    queryKey: ["posts", query],
+    queryFn: ({ pageParam = 1 }) =>
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts", {
+          params: {
+            _start: pageParam,
+            _limit: query.pageSize,
+          },
+        })
+        .then((res) => res.data),
+    staleTime: 1 * 60 * 1000,
+    getNextPageParam(lastPage, allPages) {
+      return allPages.length + 1;
+    },
+    initialPageParam: 1,
+  });
+
+export default usePosts;
