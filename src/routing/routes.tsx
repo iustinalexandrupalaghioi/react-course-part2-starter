000e6b26b242183d1579ce5dfcb58de475858893
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-03-12 17:14:23 +0200

    React router dom

---
+++ src/routing/routes.tsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./HomePage";
+import ContactPage from "./ContactPage";
+import Layout from "./Layout";
+import UsersPage from "./UsersPage";
+import UserDetail from "./UserDetail";
+import ErrorPage from "./ErrorPage";
+import LoginPage from "./LoginPage";
+import PrivateRoutes from "./PrivateRoutes";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+
+      { path: "contact", element: <ContactPage /> },
+    ],
+  },
+  {
+    element: <PrivateRoutes />,
+    children: [
+      {
+        path: "users",
+        element: <UsersPage />,
+        children: [{ path: ":id", element: <UserDetail /> }],
+      },
+    ],
+  },
+]);
+
+export default router;
